66f1429524794305400b776903df99d92c8eed23
Mischa Rouleaux <mischa-rouleaux@live.nl>
2017-08-10 14:55:09 +0200

    swoosh update 9

---
+++ app-swoosh/app-swoosh/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Mischa Rouleaux on 10-08-17.
+//  Copyright © 2017 Mischa Rouleaux. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
